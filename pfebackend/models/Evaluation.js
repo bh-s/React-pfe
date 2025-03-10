fe59276df85ee329b94248e007b493c00e4c66b2
SabineBen <sabineben779@gmail.com>
2025-03-10 15:22:01 +0100

    fix schema

--- pfebackend/models/Evaluation.js
+++ pfebackend/models/Evaluation.js
@@ -1,6 +1,11 @@
+const mongoose = require("mongoose");
+
 const OfferSchema = new mongoose.Schema({
     projectName: { type: String, required: true },
     finance: { type: String, required: true },
     duration: { type: String, required: true },
     guarantees: { type: String, required: true },
 });
+
+
+module.exports = mongoose.model("Evaluation", OfferSchema);
